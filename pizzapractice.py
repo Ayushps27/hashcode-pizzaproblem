15a14a155ad65b6d3341cf2fd5ba415734a2989b
Ayushps27 <41884241+Ayushps27@users.noreply.github.com>
2020-02-04 18:18:17 +0530

    Update pizzapractice.py

--- pizzapractice.py
+++ pizzapractice.py
@@ -2,28 +2,21 @@ def func(in_filename, out_filename):
     input_file = open(in_filename, 'r')
     output_file = open(out_filename, 'w')
     line1 = input_file.readline()
-
+    
+    #READING FILES TO RESPECTIVE VARIABLES
     slices, types = line1.split(" ")
-    slices = int(slices)
+    slices = int(slices)#type conversion from str to int
     arr = [int(i) for i in input_file.readline().split()]
 
-    # print(slices,types,arr)
-
-    arrdec = arr[::-1]
-    # print(arrdec)
-    # outslices=[]
-    outsliceindex = []
+    arrdec = arr[::-1]#reversing list to get no. of slices in devreasing order
+    outsliceindex = []#new list to store indexes of selected pizza types
 
     for i in range(len(arrdec)):
         if slices - arrdec[i] >= 0:
-            # outslices.append(i)
-            outsliceindex.append(len(arrdec) - i - 1)
+            outsliceindex.append(len(arrdec) - i - 1)#storing index of eligible pizza to the list
             slices -= arrdec[i]
-
-    # print(outslices)
-    # print(outsliceindex)
-    # outsliceindex.reverse()
-
+    
+    #WRITING OUTPUT TO FILE
     output_file.write(str(len(outsliceindex)) + "\n")
     for i in outsliceindex:
         output_file.write(str(i) + " ")
